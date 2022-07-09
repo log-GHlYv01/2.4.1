649f21911e54528316113181c32dd53ae23d081d
LesnikovYV <Y-levl@ya.ru>
2022-07-09 17:13:12 +0500

    2.4.1v2

--- 2.4/src/WrongLoginException.java
+++ 2.4/src/WrongLoginException.java
@@ -1,6 +1,7 @@
-public class WrongLoginException extends Exception {
+public class WrongLoginException extends Exception{
 
     public WrongLoginException(String message) {
         super(message);
     }
+
 }
